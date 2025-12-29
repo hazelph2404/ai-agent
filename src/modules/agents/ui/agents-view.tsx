be221d0c3957da3b94660408d294c6f79eec79a0
Hazel Pham <hazelpham2404@gmail.com>
2025-12-29 15:22:03 -0600

    feat: add empty state if data table is empty

--- src/modules/agents/ui/agents-view.tsx
+++ src/modules/agents/ui/agents-view.tsx
@@ -4,6 +4,7 @@ import { useSuspenseQuery } from '@tanstack/react-query';
 import { useTRPC } from '@/trpc/client';
 import { DataTable } from './components/data-table';
 import {columns} from './components/columns'
+import EmptyState from '@/components/empty-state';
 
 const AgentsView = () => {
   const trpc = useTRPC();
@@ -11,6 +12,7 @@ const AgentsView = () => {
   return (
     <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
         <DataTable columns={columns} data={data}/>
+        {data.length === 0 && <EmptyState title="Create your first agent" description="Create an agent to join your meeting. Each agent will follow your instructions and interact with participants during the call..."/>}
     </div>
   );
   
