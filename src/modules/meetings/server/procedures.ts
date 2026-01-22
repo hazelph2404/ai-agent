912a9a0425b2e082d5743e75daaab0682011709c
hazel <phd.h99@gmail.com>
2026-01-22 01:01:29 -0600

    Feat: Changed parameter for Generate Tokens for Meetings.
    validity_in_seconds: issuedAt ---> iat: issueAt
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -28,7 +28,8 @@ export const meetingsRouter = createTRPCRouter({
     const token = streamVideo.generateUserToken({
       user_id: ctx.auth.user.id,
       exp: expirationTime,
-      validity_in_seconds: issuedAt,
+      iat: issuedAt,
+    });
     });
 
     return token;
